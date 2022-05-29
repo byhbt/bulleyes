7ff5d39236a00cbb9e1dea65dc6e22ed446c757b
Thanh Huynh <huynhbathanh@gmail.com>
2022-05-29 14:04:27 +0700

    Initial Commit

---
+++ Bulleyes/BulleyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulleyesApp.swift
+//  Bulleyes
+//
+//  Created by Thanh Huynh on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct BulleyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
